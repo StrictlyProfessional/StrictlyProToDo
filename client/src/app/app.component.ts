5c7a0c62eb18932286772855060755a2aba685fb
mattecel <matt.ecel123@gmail.com>
2021-07-08 18:29:32 -0400

    components initial

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VaultComponent } from './components/vault/vault.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { DiscoveryComponent } from './components/discovery/discovery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { AddexerciseComponent } from './components/addexercise/addexercise.component';
+import { ExerciseComponent } from './components/exercise/exercise.component';
+import { WorkoutComponent } from './components/workout/workout.component';
+import { LeaderboardCardComponent } from './components/leaderboard-card/leaderboard-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VaultComponent,
+    LeaderboardComponent,
+    LoginComponent,
+    DiscoveryComponent,
+    NavComponent,
+    AddexerciseComponent,
+    ExerciseComponent,
+    WorkoutComponent,
+    LeaderboardCardComponent
   ],
   imports: [
     BrowserModule
