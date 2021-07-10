6803595cedf1c9d8fd3157eb2f7b7424deb5d6f6
mattecel <matt.ecel123@gmail.com>
2021-07-09 20:58:44 -0400

    navbar fix

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
+// Components
 import { LoginComponent } from './components/login/login.component';
 import { DiscoveryComponent } from './components/discovery/discovery.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 import { VaultComponent } from './components/vault/vault.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
   { path: 'discovery', component: DiscoveryComponent },
   { path: 'leaderboard', component: LeaderboardComponent },
   { path: 'vault', component: VaultComponent }
